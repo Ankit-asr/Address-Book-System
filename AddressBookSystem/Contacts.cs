c9264c294b53fb18518bc5bee01aadd96e589ada
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-08-11 23:34:24 +0530

    [Ankit] Add: Multiple Person to Address Book

--- AddressBookSystem/Contacts.cs
+++ AddressBookSystem/Contacts.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AddressBookSystem
+namespace Address_Book_System
 {
     class Contacts
     {
+        //variables
+        String firstName;
+        bool checkContacts;
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
@@ -14,13 +17,14 @@ namespace AddressBookSystem
         public int Zip { get; set; }
         public long PhoneNumber { get; set; }
         public String Email { get; set; }
-        String firstName;
-        bool checkContacts;
-
-        public void CheckContactAvailable(List<Contacts> listContacts)
+        /// <summary>
+        /// Check contact available if it presents than edit in its data
+        /// </summary>
+        /// <param name="listContacts"></param>
+        public void CheckContactAvaible(List<Contacts> listContacts)
         {
             checkContacts = false;
-            Console.WriteLine("Enter first name");
+            Console.WriteLine("Enter first name to check record");
             firstName = Console.ReadLine();
             foreach (Contacts contacts in listContacts)
             {
@@ -49,7 +53,10 @@ namespace AddressBookSystem
             if (checkContacts == false)
                 Console.WriteLine("Record not present");
         }
-        
+        /// <summary>
+        /// Delete specific record from contacts
+        /// </summary>
+        /// <param name="listContacts"></param>
         public void DeleteContacts(List<Contacts> listContacts)
         {
             checkContacts = false;
@@ -60,7 +67,7 @@ namespace AddressBookSystem
                 if (contacts.FirstName.Equals(firstName))
                 {
                     listContacts.Remove(contacts);
-                    Console.WriteLine("Record Deleted");
+                    Console.WriteLine("Record Deleted Successfully...");
                     checkContacts = true;
                     break;
                 }
@@ -68,7 +75,42 @@ namespace AddressBookSystem
             if (checkContacts == false)
                 Console.WriteLine("Record not found");
         }
-       
+        /// <summary>
+        /// Add multiple person record 
+        /// </summary>
+        /// <param name="listContacts"></param>
+        public void AddMultiplePerson(List<Contacts> listContacts)
+        {
+            Console.WriteLine("How many person do you want to add");
+            int personCount = Convert.ToInt32(Console.ReadLine());
+            for (int count = 0; count < personCount; count++)
+            {
+                Contacts contacts = new Contacts();
+                Console.WriteLine("Fill record of person :" + count + 1);
+                Console.WriteLine("Enter First Name");
+                contacts.FirstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name");
+                contacts.LastName = Console.ReadLine();
+                Console.WriteLine("Enter address");
+                contacts.Address = Console.ReadLine();
+                Console.WriteLine("Enter city");
+                contacts.City = Console.ReadLine();
+                Console.WriteLine("Enter state");
+                contacts.State = Console.ReadLine();
+                Console.WriteLine("Enter zip code");
+                contacts.Zip = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter phone number");
+                contacts.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Enter email");
+                contacts.Email = Console.ReadLine();
+                Console.WriteLine("Person added successully...");
+                listContacts.Add(contacts);
+            }
+        }
+        /// <summary>
+        /// Show all record of contacts
+        /// </summary>
+        /// <param name="listContacts"></param>
         public void ShowAllRecords(List<Contacts> listContacts)
         {
             int count = 1;
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
-namespace AddressBookSystem
+namespace Address_Book_System
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //variables
             string firstName, lastName, address, city, state, email;
             int zip, userChoice;
             long phoneNumber;
-            const int EXIT = 0, ADD_NEW_CONTACTS = 1, EDIT_CONTACTS = 2, DELETE_CONTACTS = 3, ALL_CONTACTS = 4;
+            //constants
+            const int EXIT = 0, ADD_NEW_CONTACTS = 1, EDIT_CONTACTS = 2, DELETE_CONTACTS = 3, ALL_CONTACTS = 5, ADD_MULTIPLE_RECORDS = 4;
             List<Contacts> listContacts = new List<Contacts>();
             while (true)
             {
-                Console.WriteLine("Press 1 to Add new contacts to Address Book");
-                Console.WriteLine("Press 2 to Edit existing contact");
-                Console.WriteLine("Press 3 to Delete contact");
-                Console.WriteLine("Press 4 to Show all contacts");
-                Console.WriteLine("Press 0 to to Stop Execution");
+                Console.WriteLine("Press 1 : Add new contacts to Address Book");
+                Console.WriteLine("Press 2 : Edit existing contact");
+                Console.WriteLine("Press 3 : Delete contact");
+                Console.WriteLine("Press 4 : Add multiple records to Address Book");
+                Console.WriteLine("Press 5 : Show all contacts");
+                Console.WriteLine("Press 0 : to Stop Execution");
                 Console.WriteLine("Enter your choice");
                 userChoice = Convert.ToInt32(Console.ReadLine());
                 if (userChoice == EXIT)
@@ -43,7 +45,7 @@ namespace AddressBookSystem
                         break;
                     case EDIT_CONTACTS:
                         Contacts contact = new Contacts();
-                        contact.CheckContactAvailable(listContacts);
+                        contact.CheckContactAvaible(listContacts);
                         break;
                     case ALL_CONTACTS:
                         Contacts contactAllRecord = new Contacts();
@@ -52,6 +54,10 @@ namespace AddressBookSystem
                     case DELETE_CONTACTS:
                         Contacts contectDelete = new Contacts();
                         contectDelete.DeleteContacts(listContacts);
+                        break;
+                    case ADD_MULTIPLE_RECORDS:
+                        Contacts contactAddMulRecords = new Contacts();
+                        contactAddMulRecords.AddMultiplePerson(listContacts);
                         break;
                     default:
                         Console.WriteLine("Enter a right choice");
