157fd205beac9756186a10998c7c7a167650a870
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-08-11 14:55:53 +0530

    [Ankit] Add: Contact in Address Book

---
+++ AddressBookSystem/Contacts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class Contacts
+    {
+		public String FirstName { get; set; }
+		public String Lastname { get; set; }
+		public String Address { get; set; }
+		public String City { get; set; }
+		public String State { get; set; }
+		public int Zip { get; set; }
+		public long PhoneNumber { get; set; }
+		public String Email { get; set; }
+
+		public void Contacts_Of_People()
+		{
+
+
+		}
+	}
+}
---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Address Book Program");
+            Console.WriteLine("Enter your FirstName");
+            String firstName = Console.ReadLine();
+            Console.WriteLine("Enter your Lastname");
+            String lastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address");
+            String address = Console.ReadLine();
+            Console.WriteLine("Enter your City");
+            String city = Console.ReadLine();
+            Console.WriteLine("Enter your State");
+            String state = Console.ReadLine();
+            Console.WriteLine("Enter your Phone Number");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your Zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Email ID");
+            String eMail = Console.ReadLine();
+            Contacts obj = new Contacts();
+            obj.Contacts_Of_People();
+
+        }
+    }
+}
